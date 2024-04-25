3d0b7d13dce133cd8c01ca86d8e5cf3132eba903
jaina-burk <159525249+jaina-burk@users.noreply.github.com>
2024-04-25 16:30:43 -0400

    finished

--- password_generator.py
+++ password_generator.py
@@ -10,21 +10,24 @@ def header():
 
 def generate_pass(characters=12):
     password = []
+    randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+    password.append(randomUpperLetter)
+    characters -= 1
     while characters > 0:
         case = random.randint(1,10)
-        if case <4:
+        if case < 4:
             randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
             password.append(randomLowerLetter)
             characters -= 1
-        elif case > 8:
+        elif case > 7:
             randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
             password.append(randomUpperLetter)
             characters -= 1
-        elif case == 5 or 6:
+        elif case > 4:
             randomNumber = random.randint(1,9)
             password.append(randomNumber)
             characters -= 1
-        elif case == 8 or 7:
+        elif case < 5:
             symbols = ['!','?','$']
             randomSymbol = random.choice(symbols)
             password.append(randomSymbol)
@@ -40,11 +43,28 @@ def menu():
     print("HSSEDI Password [2]")
     print("Quit [q]")
     selection_menu = input("\nEnter Selection: ")
-    while selection_menu != 'q':
+    while selection_menu != 'Q':
         if selection_menu == '1':
-            characters = int(input("Enter number of characters in password:"))
-            password = generate_pass(characters)
-            print(password)
+            characters = input("Enter number of characters in password, or q: ")
+            if characters == 'q':
+                selection_menu = 'Q'
+            else:
+                for x in range(1,6):
+                    password = generate_pass(int(characters))
+                    print(password)
+        if selection_menu == '2':
+            characters = 12
+            for x in range(1,6):
+                password = generate_pass(int(characters))
+                password += '11'
+                print(password)
+            input("Press enter to continue")
+            selection_menu = 'Q'
+        if selection_menu == 'q':
+            return 'q'
 
 
-menu()
+
+main_menu = '0'
+while main_menu != 'q':
+    main_menu = menu()
